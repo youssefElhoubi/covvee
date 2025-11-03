991f2907680207c8990209a70694f842fd8235bb
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-03 14:32:13 +0100

    started the workon the covvee web editor project

---
+++ covveeAuthService/src/main/java/com/covveeauthservice/CovveeAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.covveeauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovveeAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovveeAuthServiceApplication.class, args);
+    }
+
+}
---
+++ covveeAuthService/src/test/java/com/covveeauthservice/CovveeAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covveeauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovveeAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
