aa7f22a73ed9cc7a7043f6f7b2248ea522f7126c
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-16 22:13:23 +0100

    setign up the application gaitwai

---
+++ CovveeAPIGateway/src/main/java/com/covveeapigateway/CovveeApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.covveeapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovveeApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovveeApiGatewayApplication.class, args);
+	}
+
+}
---
+++ CovveeAPIGateway/src/test/java/com/covveeapigateway/CovveeApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covveeapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovveeApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
