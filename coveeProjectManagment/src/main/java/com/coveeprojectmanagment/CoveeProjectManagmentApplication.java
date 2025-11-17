28091580aa3fc3a5aab2aa5ba6ef335e82fe2d5e
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-17 11:22:52 +0100

    made the project management service and updated the gateway yml

---
+++ coveeProjectManagment/src/main/java/com/coveeprojectmanagment/CoveeProjectManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.coveeprojectmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoveeProjectManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoveeProjectManagmentApplication.class, args);
+    }
+
+}
---
+++ coveeProjectManagment/src/test/java/com/coveeprojectmanagment/CoveeProjectManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coveeprojectmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoveeProjectManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
