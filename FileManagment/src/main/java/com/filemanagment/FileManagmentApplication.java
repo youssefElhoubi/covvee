ed91f249849b7e7223c4a0c3027a029c806fd914
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-17 11:37:20 +0100

    added file management service

---
+++ FileManagment/src/main/java/com/filemanagment/FileManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.filemanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileManagmentApplication.class, args);
+    }
+
+}
