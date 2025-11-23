4a8a02e7af2b37a5ac7a79d63babf5f4c621bcbe
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-24 00:07:10 +0100

    feat: making the user service

--- covveeAuthService/src/main/java/com/covveeauthservice/service/UserService.java
+++ covveeAuthService/src/main/java/com/covveeauthservice/service/UserService.java
@@ -1,15 +1,29 @@
 package com.covveeauthservice.service;
 
+import com.covveeauthservice.dto.CreateUserDto;
+import com.covveeauthservice.entities.User;
 import com.covveeauthservice.mapper.UserMapper;
 import com.covveeauthservice.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class UserService {
     private UserMapper usermapper;
     private UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
 
-    public UserService(UserMapper usermapper, UserRepository userRepository) {
+    public UserService(UserMapper usermapper, UserRepository userRepository, BCryptPasswordEncoder passwordEncoder ) {
         this.usermapper = usermapper;
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
+    public CreateUserDto createUser(CreateUserDto dto){
+        userRepository.findByEmail(dto.getEmail()).orElseThrow(()->new RuntimeException("user with this email already exist"));
+        User user = usermapper.toEntity(dto);
+        userRepository.save(user);
+        return dto;
+    }
+    public User logIn(){
+        return null;
+    }
 }
