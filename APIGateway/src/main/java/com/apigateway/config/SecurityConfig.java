a0b2f737d9eabe369b33c11ac3368465ae4f6c16
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-23 22:12:38 +0100

    made the config bean in the api gateway

---
+++ APIGateway/src/main/java/com/apigateway/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange-> exchange.
+                        pathMatchers("/auth/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .build();
+    }
+}
