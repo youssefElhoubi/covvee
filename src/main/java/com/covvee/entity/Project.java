23cc2321f9cc4801b39a2a776f1d4fd58cf624e2
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-12-22 10:45:39 +0100

    created the project entity

--- src/main/java/com/covvee/entity/Project.java
+++ src/main/java/com/covvee/entity/Project.java
@@ -1,12 +1,17 @@
 package com.covvee.entity;
 
 
+import com.covvee.enums.Language;
+import com.covvee.enums.Visibility;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -18,5 +23,10 @@ public class Project {
     private String id;
     private String name;
     private String description;
-
+    private Visibility visibility;
+    private Language language;
+    @DBRef
+    private User user;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAT;
 }
