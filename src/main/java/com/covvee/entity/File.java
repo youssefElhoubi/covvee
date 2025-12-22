8e4cd5163513a8cb5d504fe347cb812455c38815
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-12-22 11:15:29 +0100

    create File and FileSystemNode entities

---
+++ src/main/java/com/covvee/entity/File.java
@@ -0,0 +1,20 @@
+package com.covvee.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document
+public class File {
+    @Id
+    private String id;
+
+}
