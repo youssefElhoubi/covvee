28ad5f326a9f39118dce3f1dd3b54f4cfb44eb3f
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-12-22 09:45:00 +0100

    started new version of covvee

---
+++ src/main/java/com/covvee/CovveeApplication.java
@@ -0,0 +1,13 @@
+package com.covvee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovveeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovveeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/covvee/CovveeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covvee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovveeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
