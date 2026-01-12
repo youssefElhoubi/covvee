5e53323ca1aa3cd58166c38c754432216da4ee4a
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-01-12 09:25:25 +0100

    made log in method in auth controller

--- src/main/java/com/covvee/controller/AuthController.java
+++ src/main/java/com/covvee/controller/AuthController.java
@@ -1,11 +1,21 @@
 package com.covvee.controller;
 
+import com.covvee.dto.auth.request.LoginRequest;
+import com.covvee.dto.auth.response.AuthResponse;
 import com.covvee.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+
+    @PostMapping()
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest dto) {
+        return ResponseEntity.ok(authService.login(dto));
+    }
 }
