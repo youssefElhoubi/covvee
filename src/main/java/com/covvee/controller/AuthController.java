7c2c4477f5dbcce801b9c73c70569fb65e2e6fa7
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-01-12 09:38:16 +0100

    made the sign up method

--- src/main/java/com/covvee/controller/AuthController.java
+++ src/main/java/com/covvee/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.covvee.controller;
 
 import com.covvee.dto.auth.request.LoginRequest;
+import com.covvee.dto.auth.request.RegisterRequest;
 import com.covvee.dto.auth.response.AuthResponse;
 import com.covvee.service.AuthService;
 import lombok.RequiredArgsConstructor;
@@ -9,13 +10,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
 
-    @PostMapping()
-    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest dto) {
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> logIn(@RequestBody LoginRequest dto) {
         return ResponseEntity.ok(authService.login(dto));
     }
+    @PostMapping("/signup")
+    public ResponseEntity<AuthResponse> signUp(@RequestBody RegisterRequest dto) {
+        return ResponseEntity.ok(authService.signUp(dto));
+    }
+
+
 }
