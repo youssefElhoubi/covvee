e9d1969d727bbf729742954794c40de074706ece
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-01-16 15:08:56 +0100

    feat: implement FolderService with CRUD operations and mapping

---
+++ src/main/java/com/covvee/service/FolderService.java
@@ -0,0 +1,59 @@
+package com.covvee.service;
+
+import com.covvee.dto.folder.request.CreateFolderRequest;
+import com.covvee.dto.folder.response.FolderResponse;
+import com.covvee.entity.Folder;
+import com.covvee.execption.ResourceNotFoundException;
+import com.covvee.mapper.FolderMapper;
+import com.covvee.repository.FolderRepository;
+import com.covvee.service.interfaces.folder.FolderInterface;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FolderService implements FolderInterface {
+    private final FolderRepository folderRepository;
+    private final FolderMapper folderMapper;
+    @Override
+    public FolderResponse createFolder(CreateFolderRequest request) {
+        Folder folder = folderMapper.toEntity(request);
+        folder = folderRepository.save(folder);
+        return folderMapper.toResponse(folder);
+    }
+
+    @Override
+    public FolderResponse getFolderById(String folderId) {
+        Folder folder = folderRepository.findById(folderId).orElseThrow(()-> {
+            throw new ResourceNotFoundException("folder was not found");
+        });
+        return folderMapper.toResponse(folder);
+    }
+
+    @Override
+    public FolderResponse renameFolder(String folderId, String newName) {
+        Folder folder = folderRepository.findById(folderId).orElseThrow(()-> {
+            throw new ResourceNotFoundException("folder was not found");
+        });
+        folder.setName(newName);
+        folder = folderRepository.save(folder);
+        return folderMapper.toResponse(folder);
+    }
+
+    @Override
+    public FolderResponse moveFolder(String folderId, String newParentId) {
+        return null;
+    }
+
+    @Override
+    public void deleteFolder(String folderId) {
+
+    }
+
+    @Override
+    public List<FolderResponse> getAllFoldersByProjectId(String projectId) {
+        return List.of();
+    }
+}
