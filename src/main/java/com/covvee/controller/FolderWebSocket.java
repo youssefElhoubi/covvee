a2b6a396de435e82a56135bb8044c4612d2afce8
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-01-19 09:36:44 +0100

    created folder creation end point

--- src/main/java/com/covvee/controller/FolderWebSocket.java
+++ src/main/java/com/covvee/controller/FolderWebSocket.java
@@ -1,7 +1,13 @@
 package com.covvee.controller;
 
+import com.covvee.dto.folder.request.CreateFolderRequest;
+import com.covvee.dto.folder.response.FolderResponse;
 import com.covvee.service.FolderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,4 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("Folder")
 public class FolderWebSocket {
     private final FolderService folderService;
+
+    @MessageMapping()
+    @SendTo("topic/folder")
+    public ResponseEntity<FolderResponse> createFolder(@Payload CreateFolderRequest request){
+        return ResponseEntity.ok(folderService.createFolder(request));
+    }
 }
