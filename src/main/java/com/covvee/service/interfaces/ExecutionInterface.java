660a60abbf12c2ad2a2e1026e199d1c691a759a9
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-01-26 12:04:51 +0100

    made execution interface

--- src/main/java/com/covvee/service/interfaces/ExecutionInterface.java
+++ src/main/java/com/covvee/service/interfaces/ExecutionInterface.java
@@ -4,4 +4,6 @@ import com.covvee.dto.ExecutionResult;
 
 public interface ExecutionInterface {
     ExecutionResult runProject(String projectId);
+    void stopExecution(String executionId);
+    boolean isEnvironmentReady();
 }
