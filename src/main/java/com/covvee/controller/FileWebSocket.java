9e0da7235256518f6dcee1a0eb654d6fd7bbcb5b
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-03-02 11:08:44 +0000

    Implement method-level security for file operations and add ProjectFileSecurity component

--- src/main/java/com/covvee/controller/FileWebSocket.java
+++ src/main/java/com/covvee/controller/FileWebSocket.java
@@ -3,12 +3,15 @@ package com.covvee.controller;
 import com.covvee.dto.file.request.RenameFileDto;
 import com.covvee.dto.file.request.UpdateFileDto;
 import com.covvee.dto.file.response.FileResponse;
+import com.covvee.security.AppUserDetails;
 import com.covvee.service.FileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,34 +28,42 @@ public class FileWebSocket {
 
     @MessageMapping()
     @SendTo("topic/data/{id}")
-    public ResponseEntity<FileResponse> updateContent(@PathVariable String id, @RequestBody UpdateFileDto content){
+    @PreAuthorize("@projectFileSecurity.ownFile(#id,userDetails)")
+    public ResponseEntity<FileResponse> updateContent(@PathVariable String id, @RequestBody UpdateFileDto content, @AuthenticationPrincipal AppUserDetails userDetails) {
         return ResponseEntity.ok(fileService.updateFileContent(id, content));
     }
 
     @MessageMapping()
     @SendTo("topic/file/{id}")
-    public ResponseEntity<FileResponse> requestFile(@PathVariable String id){
+    public ResponseEntity<FileResponse> requestFile(@PathVariable String id) {
         return ResponseEntity.ok(fileService.getFileById(id));
     }
+
     @MessageMapping()
     @SendTo("topic/rename/{id}")
-    public ResponseEntity<FileResponse> renameFile(@PathVariable String id,@RequestBody RenameFileDto content){
+    @PreAuthorize("@projectFileSecurity.ownFile(#id,userDetails)")
+    public ResponseEntity<FileResponse> renameFile(@PathVariable String id, @RequestBody RenameFileDto content, @AuthenticationPrincipal AppUserDetails userDetails) {
         return ResponseEntity.ok(fileService.renameFile(id, content));
     }
+
     @MessageMapping()
     @SendTo("topic/delete/{id}")
-    public ResponseEntity<?> deleteFile(@PathVariable String id){
+    @PreAuthorize("@projectFileSecurity.ownFile(#id,userDetails)")
+    public ResponseEntity<?> deleteFile(@PathVariable String id, @RequestBody RenameFileDto content, @AuthenticationPrincipal AppUserDetails userDetails) {
         fileService.deleteFile(id);
         return ResponseEntity.ok().build();
     }
+
     @MessageMapping()
     @SendTo("topic/move/{id}")
-    public ResponseEntity<FileResponse> moveFile(@PathVariable String id, @RequestBody String newParentFolderId ){
-        return ResponseEntity.ok(fileService.moveFile(id,newParentFolderId));
+    @PreAuthorize("@projectFileSecurity.ownFile(#id,userDetails)")
+    public ResponseEntity<FileResponse> moveFile(@PathVariable String id, @RequestBody String newParentFolderId , @AuthenticationPrincipal AppUserDetails userDetails) {
+        return ResponseEntity.ok(fileService.moveFile(id, newParentFolderId));
     }
+
     @MessageMapping()
     @SendTo("topic/project/files/{id}")
-    public ResponseEntity<List<FileResponse>> files(@PathVariable String id ){
+    public ResponseEntity<List<FileResponse>> files(@PathVariable String id) {
         return ResponseEntity.ok(fileService.getAllFilesByProjectId(id));
     }
 
