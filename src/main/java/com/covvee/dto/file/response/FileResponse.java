d48a4fba3fba8d965b1319ed915da7e7a8eba698
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-03-28 23:42:17 +0100

    fix : constructor arguments fix FileResponse

--- src/main/java/com/covvee/dto/file/response/FileResponse.java
+++ src/main/java/com/covvee/dto/file/response/FileResponse.java
@@ -1,5 +1,6 @@
 package com.covvee.dto.file.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class FileResponse {
     private String id;
     private String name;
